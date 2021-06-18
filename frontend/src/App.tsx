b6ac9376270ecd978a5a0565289d2b6c3696f4d9
Quramy <yosuke.kurami@gmail.com>
2021-06-19 02:57:52 +0900

    Change First App.tsx

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
-import React from 'react';
+import React from "react";
+import { HashRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      Hello, React!
-    </div>
+    <HashRouter>
+      <Switch>
+        <Route path="/products">
+          <div>Hello, React!</div>
+        </Route>
+        <Route>
+          <Redirect to="/products" />
+        </Route>
+      </Switch>
+    </HashRouter>
   );
 }
 
