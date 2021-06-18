df4817b87f3c1f0a1e40d13e300c3c16e7899704
Quramy <yosuke.kurami@gmail.com>
2021-06-19 02:03:42 +0900

    CRA

---
+++ frontend/src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      hello
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/index.tsx
@@ -0,0 +1,5 @@
+import { render } from "react-dom";
+import App from "./App";
+
+const element = document.getElementById("root");
+render(<App />, element!);
